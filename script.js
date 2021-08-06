bb02202c5d7156c79169ebfd4215e66ae05a3bf6
wahlnich <wahlnich@gmail.com>
2021-08-06 14:54:56 -0400

    Data Validation and rendering
    Added some data validation, as well as the ability to enter the info and render it on the page.

--- script.js
+++ script.js
@@ -2,13 +2,21 @@
 
 // Select HTML elements
 const title = document.querySelector('.render-area');
+const submit = document.querySelector('.submit-button');
+const pname = document.getElementById('pname');
+const checkboxes = document.querySelectorAll('.role-check');
 
 // Create array 
 class QueueApp {
-    constructor() {};
+    #players=['nick'];
+
+    constructor() {
+        // Add event listeners
+        submit.addEventListener('click', this.#processData.bind(this))
+    };
 
     // Get role string from array of roles
-    stringifyRoles = function(roles) {
+    #stringifyRoles = function(roles) {
         let rolesString = "Roles: ";
 
         roles.forEach(el => {
@@ -18,24 +26,85 @@ class QueueApp {
         return rolesString;
     }
 
-    renderPlayerBox = function(playerName, roles) {
+    // Add box to screen
+    #renderPlayerBox = function(playerName, roles) {
         
         // Get string for roles
-        const roleString = this.stringifyRoles(roles);
+        const roleString = this.#stringifyRoles(roles);
+
+        // Add player to #players
+        this.#players.push(playerName)
 
         // Add HTML to page
         const html = `
         <div class="player-boxes">
             <h3>Player: ${playerName}</h3>
             <h4>${roleString}</h4><br>
-            <button class="submit-button">Add to Team
+            <button class="button submit-button">Add to Team
             </button> 
         </div>
         `
         title.insertAdjacentHTML('beforeend', html)
     }
+
+    // Get values from the input box
+    #getValues = function() {
+        const playerName = pname.value;
+        const rolesArr = [];
+        
+        checkboxes.forEach(e => {
+            if(e.checked) {
+                rolesArr.push(e.value);
+            }    
+        })
+
+        return [playerName, rolesArr];
+    }
+
+    // validation helper functions
+    #dataExists = function(playerName, roles)
+    {
+        if(!playerName||!roles)
+        {
+            alert('Sorry, you must enter a player name and choose at least one role.');
+            return false;
+        }
+        else return true;
+    }
+
+    #noDuplicates = function(playerArray, playerName) {
+        let bool = true;
+
+        playerArray.forEach(el => {
+            if(el===playerName) {
+                alert('Sorry, that name already exists. please choose another name');
+                bool = false;
+            }
+        })
+        return bool;
+    }
+
+    // Validate entry
+    #validateData = function(playerArray, playerName='sdf', roles=[1,2,3]) {
+
+        // name and at least one role must exist
+        // name cannot be a duplicate
+        if(this.#dataExists(playerName, roles)&&this.#noDuplicates(playerArray, playerName)){
+            // call rendering function
+            console.log('render');
+            this.#renderPlayerBox(playerName, roles);
+        }
+    }
+
+    // Process data
+    #processData = function() {
+        // Get values from box
+        const [playerName, roles] = this.#getValues();
+
+        // Validate data
+        this.#validateData(this.#players, playerName, roles);
+
+    }
 }
 
 const app = new QueueApp();
-
-app.renderPlayerBox('redsovereign', ['controller', 'sentinel'])
