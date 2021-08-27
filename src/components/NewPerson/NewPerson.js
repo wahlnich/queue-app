33fbd23ec96559210631c7de8815872b419c8e79
wahlnich <wahlnich@gmail.com>
2021-08-27 16:25:30 -0400

    Added Checkboxes

--- src/components/NewPerson/NewPerson.js
+++ src/components/NewPerson/NewPerson.js
@@ -1,7 +1,6 @@
-import React, {useState} from "react";
+import React from "react";
 import "./NewPerson.css";
 import Card from "../UI/Card";
-import NewPersonCheckboxes from "./NewPersonCheckboxes";
 
 function NewPerson(props) {
   return (
@@ -10,7 +9,17 @@ function NewPerson(props) {
         <label>Player Name: </label>
         <input type="text"></input>
         <br />
-        <NewPersonCheckboxes />
+        <div className="checkboxes">
+          <label>Player Roles: </label>
+          <input id="ctrl" type="checkbox" value="ctrl" />
+          <label htmlFor="ctrl">Controller</label>
+          <input id="sen" type="checkbox" value="sen" />
+          <label htmlFor="sen">Sentinel</label>
+          <input id="duel" type="checkbox" value="duel" />
+          <label htmlFor="duel">Duelist</label>
+          <input id="init" type="checkbox" value="init" />
+          <label htmlFor="init">Initiator</label>
+        </div>
       </form>
     </Card>
   );
